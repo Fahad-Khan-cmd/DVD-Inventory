8ffa6d75a38787064ec5f4bc379dfc3a124602a0
Muhammad Fahad <muhammadfahad9889@gmail.com>
2024-09-09 14:08:05 +0500

    DVD inventory

--- src/DVDCollection.java
+++ src/DVDCollection.java
@@ -1,5 +1,48 @@
 public class DVDCollection {
-    public static void main(String[] args) {
+    private String name;
+    private Integer releaseYear;
+    private String director;
 
+    public DVDCollection() {
+    }
+
+    public DVDCollection(String name, Integer releaseYear, String director) {
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.director = director;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setReleaseYear(Integer releaseYear)
+    {
+        this.releaseYear = releaseYear;
+    }
+    public Integer getReleaseYear()
+    {
+        return releaseYear;
+    }
+    public void setDirector(String director)
+    {
+        this.director = director;
+    }
+    public String getDirector()
+    {
+        return director;
+    }
+
+    @Override
+    public String toString() {
+        return "DVDInventory{" +
+                "name='" + name + '\'' +
+                ", releaseYear='" + releaseYear + '\'' +
+                ", director='" + director + '\'' +
+                '}';
     }
 }
